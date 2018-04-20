ff98d6a4b048a6f1b2468435ade406076c1b1d37
mngdnmw <mtdunmow@gmail.com>
2018-04-20 11:04:53 +0200

    Added database

--- public/index.php
+++ public/index.php
@@ -1,11 +1,35 @@
 <!doctype html>
-
 <?php include 'templates/header.php'; ?>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-6">
-        <button class ="btn btn-success">OKAY</button>
-      </div>
+<section id="plans">
+    <div class="container">
+        <h5 class="section-title h1">PLAN OVERVIEW</h5>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4">
+                <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                    <div class="mainflip">
+                        <div class="frontside">
+                            <div class="card">
+                                <div class="card-body text-center">
+                                    <p><img class=" img-fluid" src="https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_06.jpg" alt="card image"></p>
+                                    <h4 class="card-title">Paul's Plan</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="backside">
+                            <div class="card">
+                                <div class="card-body text-center mt-4">
+                                    <ul class="list-group">
+                                        <li class="list-group-item">Leg Day</li>
+                                        <li class="list-group-item">Upper Arm Day</li>
+                                    </ul>
+                                    <a href="plan_detail.php" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+</section>
 <?php include 'templates/footer.php'; ?>
---
+++ public/plan_detail.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<?php include 'templates/header.php'; ?>
+<section id="plan_details">
+    <div class="container">
+        <h5 class="section-title h1">Paul's Plan</h5>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-8">
+                <div class="card">
+                    <div class="card-body text-center mt-4">
+                        <p>Leg Day</p>
+                        <ul class="list-group">
+                            <li class="list-group-item">Squat</li>
+                            <li class="list-group-item">Running</li>
+                        </ul>
+                        <a href="plan_detail.php" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                <p>Stuff</p>
+                <ul class="list-group">
+                    <li class="list-group-item">Squat</li>
+                    <li class="list-group-item">Running</li>
+                </ul>
+            </div>
+        </div>
+        <a href="plan_detail.php" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i></a>
+    </div>
+    </div>
+</section>
+<?php include 'templates/footer.php'; ?>
