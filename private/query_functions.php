bac7402509d9e24b34dc63354c26e6e0c62b7877
mngdnmw <mtdunmow@gmail.com>
2018-04-22 12:23:25 +0200

    Delete function works

--- private/query_functions.php
+++ private/query_functions.php
@@ -3,7 +3,8 @@
 function get_all_workouts($connection)
 {
     try {
-        $sql = "SELECT * FROM plan";
+        $sql = "SELECT * ";
+        $sql .= "FROM `plan`;";
         $plan_statement = $connection->prepare($sql);
         $plan_statement->execute();
         $plan_result = $plan_statement->fetchAll();
@@ -17,7 +18,10 @@ function get_all_workouts($connection)
 function get_workout_days($connection, $plan_id)
 {
     try {
-        $sql = "SELECT * FROM plan_days WHERE plan_id =:plan_id ORDER BY `order`";
+        $sql = "SELECT * ";
+        $sql .= "FROM `plan_days` ";
+        $sql .="WHERE plan_id =:plan_id ";
+        $sql .="ORDER BY `order`;";
         $day_statement = $connection->prepare($sql);
         $day_statement->bindParam(':plan_id', $plan_id, PDO::PARAM_STR);
         $day_statement->execute();
@@ -33,17 +37,19 @@ function get_workout_days($connection, $plan_id)
 function delete_workout($connection, $plan_id)
 {
     try {
-        $sql = "DELETE FROM plan WHERE id =:plan_id ";
-        $sql .= "LIMIT 1";
+
+        $sql = "DELETE p, pd ";
+        $sql .= "FROM `plan` p ";
+        $sql .= "JOIN `plan_days` pd ON pd.plan_id = p.id ";
+        $sql .= "WHERE p.id =:plan_id;";
         $delete_statement = $connection->prepare($sql);
         $delete_statement->bindParam(':plan_id', $plan_id, PDO::PARAM_INT);
         $delete_statement->execute();
         $count = $delete_statement->rowCount();
-        if($count>0)
-        {
+        if ($count > 0) {
             echo '<script>console.log("deleted plan!")</script>';
             return true;
-        }else{
+        } else {
             echo '<script>console.log("Could not delete plan!")</script>';
             return false;
         }
